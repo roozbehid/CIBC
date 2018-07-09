e7e23c04f0c02de1a655bcc9ee99e42e07f41166
roozbeh@homewin10.com <roozbeh@homewin10.com>
2018-07-08 22:46:17 -0600

    With better comments

--- Task2/Task3.py
+++ Task2/Task3.py
@@ -1,37 +1,37 @@
 # ref : https://www.pyimagesearch.com/2014/09/15/python-compare-two-images/
 import cv2
 import sys
+import pytest
 from skimage.measure import compare_ssim, compare_nrmse
 
-imA = cv2.imread(sys.argv[1])
-imB = cv2.imread(sys.argv[2])
+def printImageDiff(img1Path, img2Path):
+    imA = cv2.imread(img1Path)
+    imB = cv2.imread(img2Path)
 
-grayA = cv2.cvtColor(imA, cv2.COLOR_BGR2GRAY)
-grayB = cv2.cvtColor(imB, cv2.COLOR_BGR2GRAY)
+    grayA = cv2.cvtColor(imA, cv2.COLOR_BGR2GRAY)
+    grayB = cv2.cvtColor(imB, cv2.COLOR_BGR2GRAY)
 
-(score, grad) = compare_ssim(grayA, grayB, full=True)
-grad = (grad * 255).astype("uint8")
-print("SSIM: {}".format(score))
+    (score, grad) = compare_ssim(grayA, grayB, full=True)
+    #grad = (grad * 255).astype("uint8")
 
-nrmse = compare_nrmse(grayA, grayB)
-print("NRMSE: {}".format(nrmse))
+    nrmse = compare_nrmse(grayA, grayB)
 
-thresh = cv2.threshold(grad, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] 
+    return (score, nrmse)
+    #thresh = cv2.threshold(grad, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    # show the output images
+    #cv2.imshow("Image 1", imA)
+    #cv2.imshow("Image 2", imB)
+    #cv2.imshow("Diff", grad)
+    #cv2.imshow("Thresh", thresh)
+    #cv2.waitKey(0)
 
-# loop over the contours
-for c in cnts:
-	# compute the bounding box of the contour and then draw the
-	# bounding box on both input images to represent where the two
-	# images differ
-	(x, y, w, h) = cv2.boundingRect(c)
-	cv2.rectangle(imA, (x, y), (x + w, y + h), (0, 0, 255), 2)
-	cv2.rectangle(imB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+def test_same_image():
+    (ssim, nrmse) = printImageDiff(".\\data\\diff_test\\test_image_1.png",".\\data\\diff_test\\test_image_1.png")
+    assert abs(ssim - 1.0) < 0.01
+    assert abs(nrmse - 0.0) < 0.01
 
-# show the output images
-cv2.imshow("Image 1", imA)
-cv2.imshow("Image 2", imB)
-cv2.imshow("Diff", grad)
-cv2.imshow("Thresh", thresh)
-cv2.waitKey(0)
+(ssim, nrmse) = printImageDiff(sys.argv[1],sys.argv[1])
+print("structural similarity (SSIM) index is: %f" % ssim)
+print("normalized root mean square is: %f" % nrmse)
+
+
